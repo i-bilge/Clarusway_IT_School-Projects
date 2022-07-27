99ca42edbf040d2dc77a656cb7ed5c2d0823c62c
i-bilge <iismailbilge@gmail.com>
2022-07-27 13:38:35 +0200

    styling is finished and aoi link added.

--- 012-Movie-App-React/movie-app/src/components/Card.js
+++ 012-Movie-App-React/movie-app/src/components/Card.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Card() {
+function Card(movie) {
   return (
     <>
         <div className='movie'>
@@ -8,7 +8,7 @@ function Card() {
             <div className='movie-details'>
                 <div className='box'>
                     <h4 className='title'>Movie Title</h4>
-                    <p className='rating'>info here</p>
+                    <p className='rating'>9,5</p>
                 </div>
                 <div className='overview'>
                     <h1>
