58851819d29efbba3f8b37f2cdb047d1760b2b58
i-bilge <iismailbilge@gmail.com>
2022-07-27 13:39:47 +0200

    styling finished and Api added.

--- 012-Movie-App-React/movie-app/src/components/Main.js
+++ 012-Movie-App-React/movie-app/src/components/Main.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Card from './Card'
-
+let API_key="&api_key=5e5ba60b460ceadbd75b3baff60a87de";
+let base_url="https://api.themoviedb.org/3";
+let url= base_url+"/discover/movie?sort_by=popularity.desc"+API_key;
 
 function Main() {
+    const [movieData, setData] = useState([]);
+    const [url_set, setUrl] = useState(url);
+
+    useEffect(() => {
+        fetch(url_set).then(res=>res.json()).then(data => {
+            // console.log(data.results)
+            setData(data.results);
+        })
+    }, [url_set])
   return (
     <>
     <div className='header'>
@@ -24,9 +35,16 @@ function Main() {
             </div>
         </form>
     </div>
-    <idv className='container'>
-        <Card />
-    </idv>
+    <div className='container'>
+        {
+          (movieData.length==0)?<p className='notfound'>Not Found</p>:
+          movieData.map(()=> {
+            return(
+                <Card/>
+            )
+          })
+        }
+    </div>
     </>
   )
 }
